0af901949278c17f441089ca793572ee6e5672c1
dh73111 <dh73111@gmail.com>
2019-11-26 16:26:00 +0900

    first commit

---
+++ src/cloud_pj/awsTest.java
@@ -0,0 +1,104 @@
+package cloud_pj;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.DescribeImagesRequest;
+import com.amazonaws.services.ec2.model.DescribeImagesResult;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.ec2.model.RebootInstancesRequest;
+import com.amazonaws.services.ec2.model.RebootInstancesResult;
+import com.amazonaws.services.ec2.model.Region;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import java.util.Scanner;
+
+public class awsTest {
+	/*
+	 * Cloud Computing, Data Computing Laboratory Department of Computer Science
+	 * Chungbuk National University
+	 */
+	static AmazonEC2 ec2;
+
+	private static void init() throws Exception {
+		/*
+		 * The ProfileCredentialsProvider will return your [default] credential profile
+		 * by reading from the credentials file located at (~/.aws/credentials).
+		 */
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+		try {
+			credentialsProvider.getCredentials();
+		} catch (Exception e) {
+			throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
+					+ "Please make sure that your credentials file is at the correct "
+					+ "location (~/.aws/credentials), and is in valid format.", e);
+
+		}
+		ec2 = AmazonEC2ClientBuilder.standard().withCredentials(credentialsProvider).withRegion("us-east-1") /*
+																												*/
+				.build();
+	}
+
+	public static void main(String[] args) throws Exception {
+		init();
+		Scanner menu = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
+		Scanner id_string = new Scanner(System.in);
+		int number = 0;
+		while (true) {
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println("------------------------------------------------------------");
+			System.out.println(" Amazon AWS Control Panel using SDK ");
+			System.out.println(" ");
+			System.out.println(" Cloud Computing, Computer Science Department ");
+			System.out.println(" at Chungbuk National University ");
+			System.out.println("------------------------------------------------------------");
+			System.out.println(" 1. list instance 2. available zones ");
+			System.out.println(" 3. start instance 4. available regions ");
+			System.out.println(" 5. stop instance 6. create instance ");
+			System.out.println(" 7. reboot instance 8. list images ");
+			System.out.println(" 99. quit ");
+			System.out.println("------------------------------------------------------------");
+			System.out.print("Enter an integer: ");
+			number = scan.nextInt();
+			switch (number) {
+			case 1:
+				listInstances();
+				break;
+			}
+		}
+	}
+
+	public static void listInstances() {
+		System.out.println("Listing instances....");
+		boolean done = false;
+		DescribeInstancesRequest request = new DescribeInstancesRequest();
+		while (!done) {
+			DescribeInstancesResult response = ec2.describeInstances(request);
+			for (Reservation reservation : response.getReservations()) {
+				for (Instance instance : reservation.getInstances()) {
+					System.out.printf(
+							"[id] %s, " + "[AMI] %s, " + "[type] %s, " + "[state] %10s, " + "[monitoring state] %s",
+							instance.getInstanceId(), instance.getImageId(), instance.getInstanceType(),
+							instance.getState().getName(), instance.getMonitoring().getState());
+				}
+				System.out.println();
+			}
+			request.setNextToken(response.getNextToken());
+			if (response.getNextToken() == null) {
+				done = true;
+			}
+		}
+	}
+}
